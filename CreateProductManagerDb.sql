39344f1562d8da45259fbe4e4b946d1f49fd1b6f
Justin <justinburns2008@gmail.com>
2025-11-25 11:30:24 -0800

    Added SQL script for ProductManagerDb database

---
+++ CreateProductManagerDb.sql
@@ -0,0 +1,18 @@
+﻿-- Drop the ProductManagerDb database if it exists.
+DROP DATABASE IF EXISTS ProductManagerDb;
+GO
+
+-- Create a new ProductManagerDb database.
+CREATE DATABASE ProductManagerDb;
+GO
+
+-- Switch to the newly created ProductManagerDb database.
+USE ProductManagerDb;
+GO
+
+CREATE TABLE dbo.Products
+(
+    Id         INT              NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    SalesPrice DECIMAL(10,2)    NOT NULL CHECK (SalesPrice >= 0),
+    Name       VARCHAR(200)     NOT NULL
+);
